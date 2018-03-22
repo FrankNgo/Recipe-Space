8e8418e3cdf91a8ad93b5150a166634f15e85cda
Frank Ngo <frankngomusic@gmail.com>
2018-03-22 11:54:59 -0700

    add promises

--- src/NutritionalRecipes.js
+++ src/NutritionalRecipes.js
@@ -1,12 +1,11 @@
-export function recipe_search(recipe) {
-  $.ajax({
-    url: `https://api.edamam.com/search?q=${recipe}&app_id=75530c1e&app_key=622c789e025e095b75e9f91956cb7cb6`,
-    type: 'GET',
-    data: {
-      format: 'json'
-    },
-    success: function(results) {
-      var recipes = results.hits;
+export class RecipeApi {
+  constructor(recipe) {
+    this.recipe = recipe;
+  }
+  getRecipes(recipe) {
+    return $.get( `https://api.edamam.com/search?q=${recipe}&app_id=75530c1e&app_key=622c789e025e095b75e9f91956cb7cb6`)
+    .then(function(response) {
+      var recipes = response.hits;
       for (var i = 0; i < recipes.length; i++) {
         var recipe = recipes[i].recipe;
         var title = recipe.label;
@@ -21,11 +20,7 @@ export function recipe_search(recipe) {
         var recipe_title = $('<h3>').html(title);
         var recipe_image = $('<img>').attr('src', image_url);
         var label = $('<h4>').html('Ingredients');
-        $("#recipes").append(recipe_title, recipe_image, label, ingredientList);
       }
-    },
-    error: function() {
-      $('#errors').text("There was an error processing your request. Please try again.")
-    }
-  });
+    })
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { recipe_search } from './NutritionalRecipes.js';
+import { RecipeApi } from './NutritionalRecipes.js';
 
 $(document).ready(function() {
+
+  function parseResponse(title){
+    $("#recipes").text(title);
+    //  , recipe_image, label, ingredientList);
+  }
+
   $('#recipeSearch').click(function() {
+    event.preventDefault();
     let recipe = $('#recipe').val();
-    $('#recipe').val("");
-    recipe_search(recipe);
+    let recipe_api = new RecipeApi(recipe);
+
+    let promise = recipe_api.getRecipes();
+    $("#recipes").empty();
+
+    promise.then(function(response) {
+      parseResponse(response);
+    },function(error){
+      $("#output").text(error);
+    });
   });
+
 });
