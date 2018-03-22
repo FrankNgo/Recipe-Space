ad0175dafecf320696c66ad8edfa33395ccbaa6e
Frank Ngo <frankngomusic@gmail.com>
2018-03-22 15:14:54 -0700

    add promises

--- src/NutritionalRecipes.js
+++ src/NutritionalRecipes.js
@@ -1,26 +1,57 @@
-export class RecipeApi {
-  constructor(recipe) {
-    this.recipe = recipe;
-  }
-  getRecipes(recipe) {
-    return $.get( `https://api.edamam.com/search?q=${recipe}&app_id=75530c1e&app_key=622c789e025e095b75e9f91956cb7cb6`)
-    .then(function(response) {
-      var recipes = response.hits;
-      for (var i = 0; i < recipes.length; i++) {
-        var recipe = recipes[i].recipe;
-        var title = recipe.label;
-        var image_url = recipe.image;
-        var ingredients = recipe.ingredients;
-        var ingredientList = $('<ul>');
-        for (var j = 0; j < ingredients.length; j++) {
-          var ingredient = ingredients[j].text;
-          var li = $('<li>').html(ingredient);
-          ingredientList.append(li);
-        }
-        var recipe_title = $('<h3>').html(title);
-        var recipe_image = $('<img>').attr('src', image_url);
-        var label = $('<h4>').html('Ingredients');
-      }
+import $ from 'jquery';
+
+export class RecipePick {
+
+  // constructor(userInput) {
+  //   this.recipes = userInput;
+  //   }
+
+  getRecipes(userInput, displayData) {
+     $.get( `https://api.edamam.com/search?q=${userInput}&app_id=75530c1e&app_key=622c789e025e095b75e9f91956cb7cb6`)
+     .then(response => {
+      // let recipes = this.hits;
+      // let title = this.label;
+      console.log(response.hits);
+      displayData(response);
     })
+    .fail(() => 'something went wronggggg')
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+//     .then(function(response) {
+      //
+      // for (let i = 0; i < recipes.length; i++) {
+      //   let recipe = recipes[i].recipe;
+      //   let title = recipe.label;
+      //   let image_url = recipe.image;
+      //   let ingredients = recipe.ingredients;
+      //   let ingredientList = $('<ul>');
+      //   for (let j = 0; j < ingredients.length; j++) {
+      //     let ingredient = ingredients[j].text;
+      //     let li = $('<li>').html(ingredient);
+      //     ingredientList.append(li);
+      //   }
+//         let recipe_title = $('<h3>').html(title);
+//         let recipe_image = $('<img>').attr('src', image_url);
+//         let label = $('<h4>').html('Ingredients');
+//
+//         let parsingResponse = function parseResponse(title){
+//           $("#recipes").text(title);
+//           //  , recipe_image, label, ingredientList);
+//         }
+//         console.log(parsingResponse);
+//         return parsingResponse;
+//       }
+//     })
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,46 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { RecipeApi } from './NutritionalRecipes.js';
+import { RecipePick } from './NutritionalRecipes.js';
 
-$(document).ready(function() {
 
-  function parseResponse(title){
-    $("#recipes").text(title);
-    //  , recipe_image, label, ingredientList);
-  }
-
-  $('#recipeSearch').click(function() {
-    event.preventDefault();
-    let recipe = $('#recipe').val();
-    let recipe_api = new RecipeApi(recipe);
-
-    let promise = recipe_api.getRecipes();
-    $("#recipes").empty();
-
-    promise.then(function(response) {
-      parseResponse(response);
-    },function(error){
-      $("#output").text(error);
-    });
+const displayData = response => {
+  console.log(response.hits);
+  response.hits.forEach(hit => {
+    console.log(hit);
+    console.log(hit.recipe);
+    console.log(hit.recipe.label);
+    $('#recipes').append(`<li>${hit.recipe.label}</li>`);
   });
+};
 
+$(document).ready(() => {
+    let recipeList = new RecipePick();
+
+    $('#recipeSearch').click(e => {
+    $('#recipes').empty()
+    e.preventDefault();
+    let userInput = $('#recipe').val()
+    RecipePick.prototype.getRecipes(userInput, displayData)
+
+  });
 });
+
+
+// $(document).ready(function() {
+//   $('#recipeSearch').click(function() {
+//     event.preventDefault();
+//     let recipe = $('#recipe').val();
+//     let recipe_api = new RecipeApi(recipe);
+//
+//     let promise = recipe_api.getRecipes();
+//     $("#recipes").empty();
+//
+//     promise.then(function(response) {
+//       parseResponse(response);
+//     },function(error){
+//       $("#output").text(error);
+//     });
+//   });
+//
+// });
